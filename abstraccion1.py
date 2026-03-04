b29422f930e95120bad98ef99e1acfad95e09ebe
MAPSmiguel <miguel.piedrahita3@udea.edu.co>
2026-03-04 10:35:57 -0500

    Version 2

--- abstraccion1.py
+++ abstraccion1.py
@@ -26,41 +26,45 @@ class Paciente():
 class Sistema():
     def __init__(self):
         self.__listado = []
-    def ingresarPaciente(self):
-        nombre = input('Ingrese el nombre del paciente: ')
-        cedula = int(input('Ingrese la cedula del paciente: '))
-        genero = input('Ingrese el genero del paciente: ')
-        servicio = input('Ingrese el servicio del paciente: ')
-        paciente = Paciente()
-        paciente.setNombre(nombre)
-        paciente.setCedula(cedula)
-        paciente.setGenero(genero)
-        paciente.setServicio(servicio)
+    def ingresarPaciente(self, paciente:Paciente):
         self.__listado.append(paciente)
     def numeroPacientes(self):
-        return len(self.__listado)
-    def datosPaciente(self):
-        cedula = int(input('Ingrese la cédula a buscar: '))
+        print(f'El número de pacientes es: {len(self.__listado)}')
+    def datosPaciente(self, cedula:int):
         for paciente in self.__listado:
             if paciente.getCedula() == cedula:
-                print(f'Nombre: {paciente.getNombre()}')
-                print(f'Cédula: {paciente.getCedula()}')
-                print(f'Género: {paciente.getGenero()}')
-                print(f'Servicio: {paciente.getServicio()}')
-                return
+                return paciente
         print('Paciente no encontrado.')
 
-sistema = Sistema()
+def main():
+    sistema = Sistema()
+    while True:
+        opcion = int(input('Ingrese una opción (1: Ingresar paciente, 2: Número de pacientes, 3: Datos de paciente, 4: Salir): '))
+        if opcion == 1:
+            print('A continuación se solicitarán los datos del paciente.')
+            nombre = input('Ingrese el nombre del paciente: ')
+            cedula = int(input('Ingrese la cédula del paciente: '))
+            genero = input('Ingrese el género del paciente: ')
+            servicio = input('Ingrese el servicio requerido por el paciente: ')
+            paciente = Paciente()
+            paciente.setNombre(nombre)
+            paciente.setCedula(cedula)
+            paciente.setGenero(genero)
+            paciente.setServicio(servicio)
+            sistema.ingresarPaciente(paciente)
+        elif opcion == 2:
+            sistema.numeroPacientes()
+        elif opcion == 3:
+            cedula = int(input('Ingrese la cédula del paciente a buscar: '))
+            paciente = sistema.datosPaciente(cedula)
+            print(f'Nombre: {paciente.getNombre()}')
+            print(f'Cédula: {paciente.getCedula()}')
+            print(f'Género: {paciente.getGenero()}')
+            print(f'Servicio: {paciente.getServicio()}')
+        elif opcion == 4:
+            break
+        else:
+            print('Opción no válida. Intente nuevamente.')
 
-while True:
-    opcion = int(input('Ingrese una opción (1: Ingresar paciente, 2: Número de pacientes, 3: Datos de paciente, 4: Salir): '))
-    if opcion == 1:
-        sistema.ingresarPaciente()
-    elif opcion == 2:
-        print(f'El número de pacientes es: {sistema.numeroPacientes()}')
-    elif opcion == 3:
-        sistema.datosPaciente()
-    elif opcion == 4:
-        break
-    else:
-        print('Opción no válida. Intente nuevamente.')
+if __name__ == '__main__':
+    main()
