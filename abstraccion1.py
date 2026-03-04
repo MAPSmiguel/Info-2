e2cf3a9cb79a93dab4413f181c165e159e621ee9
MAPSmiguel <miguel.piedrahita3@udea.edu.co>
2026-03-04 11:07:27 -0500

    Versión 3. Verificaciones y nuevo método para buscar

--- abstraccion1.py
+++ abstraccion1.py
@@ -27,14 +27,29 @@ class Sistema():
     def __init__(self):
         self.__listado = []
     def ingresarPaciente(self, paciente:Paciente):
+        for p in self.__listado:
+            if p.getCedula() == paciente.getCedula():
+                print('Ya existe un paciente con esa cédula.')
+                return None
         self.__listado.append(paciente)
     def numeroPacientes(self):
         print(f'El número de pacientes es: {len(self.__listado)}')
-    def datosPaciente(self, cedula:int):
-        for paciente in self.__listado:
-            if paciente.getCedula() == cedula:
-                return paciente
-        print('Paciente no encontrado.')
+    def datosPaciente(self, z):
+        resultado = []
+        if isinstance(z, int):
+            for paciente in self.__listado:
+                if paciente.getCedula() == z:
+                    resultado.append(paciente)
+                    return resultado
+            print('Paciente no encontrado.')
+        else:
+            for paciente in self.__listado:
+                if z in paciente.getNombre():
+                    resultado.append(paciente)
+            if len(resultado) > 0:
+                return resultado
+            else:
+                print('Paciente no encontrado.')
 
 def main():
     sistema = Sistema()
@@ -55,12 +70,19 @@ def main():
         elif opcion == 2:
             sistema.numeroPacientes()
         elif opcion == 3:
-            cedula = int(input('Ingrese la cédula del paciente a buscar: '))
-            paciente = sistema.datosPaciente(cedula)
-            print(f'Nombre: {paciente.getNombre()}')
-            print(f'Cédula: {paciente.getCedula()}')
-            print(f'Género: {paciente.getGenero()}')
-            print(f'Servicio: {paciente.getServicio()}')
+            metodo = int(input('Ingrese el método de búsqueda (1: Cédula): (2: Nombre): '))
+            if metodo == 1:
+                cedula = int(input('Ingrese la cédula del paciente a buscar: '))
+                pacientes = sistema.datosPaciente(cedula)
+            elif metodo == 2:
+                nombre = input('Ingrese el nombre del paciente a buscar: ')
+                pacientes = sistema.datosPaciente(nombre)
+            if pacientes is not None:
+                for paciente in pacientes:
+                    print(f'Nombre: {paciente.getNombre()}')
+                    print(f'Cédula: {paciente.getCedula()}')
+                    print(f'Género: {paciente.getGenero()}')
+                    print(f'Servicio: {paciente.getServicio()}')
         elif opcion == 4:
             break
         else:
